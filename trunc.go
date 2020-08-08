e01fa4f3fefc5eda5dde490cfd3c145955b2e042
John Jacob <jacobjohnrobert@gmail.com>
2020-08-08 11:07:10 -0500

    Initial commit.

---
+++ trunc.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+)
+
+func main() {
+	var length = flag.Int("length", 80, "Length to truncate lines to.")
+	var filename = flag.String("file", "", "Name of file to truncate.")
+	flag.Parse()
+
+	if *filename == "" {
+		fmt.Printf("%v", *filename)
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+
+	dat, err := ioutil.ReadFile(*filename)
+	if err != nil {
+		panic(err)
+	}
+
+	lines := strings.Split(string(dat), "\n")
+	for _, line := range lines {
+		chars := []rune(line)
+		oldLength := len(chars)
+		var newLines = make([]string, 0)
+		for i := 0; i < oldLength; i += *length {
+			var stopIndex int
+			if i + *length < oldLength {
+				stopIndex = i + *length
+			} else {
+				stopIndex = oldLength
+			}
+			newLines = append(newLines, string(chars[i:stopIndex]))
+		}
+		newLineStr := strings.Join(newLines, "\n")
+		fmt.Println(newLineStr)
+	}
+}
